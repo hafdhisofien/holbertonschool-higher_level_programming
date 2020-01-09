28527047d1e41ee886f220565c6c0f28da32d8eb
hafdhisofien <1114@holbertonschool.com>
2020-01-09 15:24:22 +0000

    fix pep8

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 class Square:
-     """" A simple Square"""
+    """" A simple Square"""
 
-     def __init__(self, size):
-          """Example function with types documented in the docstring.
-          `PEP 484`_ type annotations are supported. If attribute, parameter, and
-          return types are annotated according to `PEP 484`_, they do not need to be
-          included in the docstring:
-          Args:
-          self: self.
-          size: Private instance attribute.
-          """
-          self.__size = size
+    def __init__(self, size):
+        """ init like a constructor .
+        Args:
+        self: self.
+        size: Private instance attribute.
+        """
+        self.__size = size
