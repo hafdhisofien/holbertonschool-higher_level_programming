da5040b939e2f75c8451319e8d01b1209c3de215
hafdhisofien <1114@holbertonschool.com>
2020-01-09 15:32:42 +0000

    pep8 fixes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 class Square:
-     """" A simple Square"""
+    """" A simple Square"""
 
-     def __init__(self, size=0):
-          """Example function with PEP 484 type annotations.
-          Args:
-          param1: The first parameter.
-          param2: The second parameter.
-          Returns:
-          The return value. True for success, False otherwise.
-          """
-          self.__size = size
-          try:
-              size += 1
-              if (size<0):
-                  raise ValueError
-          except ValueError:
-              raise Exception("size must be >= 0")
-          except TypeError:
-              raise Exception("size must be an integer")
+    def __init__(self, size=0):
+        """init: Instantiation.
+        Args:
+        self: this square.
+        size: size of my_sqaure.
+        Returns:
+        None.
+        """
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
