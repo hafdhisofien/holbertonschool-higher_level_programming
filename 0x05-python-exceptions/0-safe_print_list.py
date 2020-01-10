8be00b8adc0dffd64d35c913f77ec50c0d9e3fd7
hafdhisofien <1114@holbertonschool.com>
2020-01-10 09:17:16 +0000

    pep8 fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    z=0
+    z = 0
     try:
         for i in range(x):
             print("{:d}".format(my_list[i]), end="")
-            z+=1
+            z += 1
     except:
         print('')
         return(z)
