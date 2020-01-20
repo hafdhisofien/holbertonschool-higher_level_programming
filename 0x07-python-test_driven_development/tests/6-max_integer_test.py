0cca54041b1d20b0090062791501dfa66ca5d44d
hafdhisofien <1114@holbertonschool.com>
2020-01-20 10:14:25 +0000

    fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,40 +6,40 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     
-    def empty_list_test(self):
+    def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
 
-    def max_front(self):
+    def test_max_front(self):
         my_list = [5, 2, 3, 4]
         self.assertEqual(max_integer(my_list), 5)
 
-    def max_middle(self):
+    def test_max_middle(self):
         my_list = [5, 2, 99, 3, 4]
         self.assertEqual(max_integer(my_list), 99)
 
-    def max_end(self):
+    def test_max_end(self):
         my_list = [1, 2, 3, 4, 56]
         self.assertEqual(max_integer(my_list), 56)
 
-    def max_long(self):
+    def test_max_long(self):
         my_list = [1, 2, 3, 4, 5, 77, 88, 99, 0, 56]
         self.assertEqual(max_integer(my_list), 99)
 
-    def negative(self):
+    def test_negative(self):
         my_list = [-1, -22, -3, -4]
         self.assertEqual(max_integer(my_list), -1)
 
-    def mixed_cases(self):
+    def test_mixed_cases(self):
         my_list = [2, 3, 4, 5, 'd']
         self.assertEqual(max_integer(my_list), "d")
 
-    def isnone(self):
+    def test_isnone(self):
         my_list = []
         self.assertEqual(max_integer(my_list), None)
 
-    def none(self):
+    def test_none(self):
         with self.assertRaises(TypeError):
             max_integer(None)
 
-    def string(self):
+    def test_with_string(self):
         self.assertEqual(max_integer("string"), "t")
