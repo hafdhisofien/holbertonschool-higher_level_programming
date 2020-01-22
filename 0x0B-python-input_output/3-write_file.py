9eafd4d8f0943cf0941152bf484719379cedb3de
hafdhisofien <1114@holbertonschool.com>
2020-01-22 16:33:27 +0000

    pep8 fix

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def write_file(filename="", text=""):
     """
-    function that writes a string to a text file (UTF8) 
+    function that writes a string to a text file (UTF8)
     and returns the number of characters written
     Args:
     filename: current file name.
