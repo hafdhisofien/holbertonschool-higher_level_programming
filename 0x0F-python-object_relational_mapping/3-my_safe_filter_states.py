da7dc8143da9eb939978ac65691b8f7a47538aa7
hafdhisofien <1114@holbertonschool.com>
2020-03-29 16:02:05 +0000

    o

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,7 +10,7 @@ from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
+                         passwd=argv[2], db=argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC",
                    (argv[4],))
