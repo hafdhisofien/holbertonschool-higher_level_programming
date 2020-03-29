b19a850354373f6fed03151034725bff218ed41c
hafdhisofien <1114@holbertonschool.com>
2020-03-29 20:15:13 +0000

    typo

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     JOIN states \
     ON cities.state_id = states.id \
     WHERE states.name LIKE %s \
-    ORDER BY cities.id", (argv[4],))
+    ORDER BY cities.id", (argv[4]))
     query_rows = cursor.fetchall()
     print(", ".join(row[0] for row in query_rows))
     cursor.close()
