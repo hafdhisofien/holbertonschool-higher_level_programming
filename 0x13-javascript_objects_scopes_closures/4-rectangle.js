868b102bfb9de707fd7dd61f109dbf55884b9658
hafdhisofien <1114@holbertonschool.com>
2020-05-11 19:33:22 +0000

    semistandard

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -6,17 +6,20 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
+
   rotate () {
     const w = this.width;
     const h = this.height;
     this.width = h;
     this.height = w;
   }
+
   double () {
     this.width *= 2;
     this.height *= 2;
